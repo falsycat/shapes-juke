84b003379043d2aaea7945d32d4ac2d096b5b953
falsycat <me@falsy.cat>
2019-10-05 00:00:00 +0000

    [add] Added ElementInterface.

---
+++ sjplayer/src/sjplayer/ElementInterface.d
@@ -0,0 +1,26 @@
+/// License: MIT
+module sjplayer.ElementInterface;
+
+import gl4d;
+
+///
+interface ElementInterface {
+ public:
+  ///
+  static struct DamageCalculationResult {
+    /// An amount of the damage. (0~1)
+    float damage;
+    /// A nearness of the point. (0~1)
+    float nearness;
+  }
+
+  ///
+  DamageCalculationResult CalculateDamage(vec2 p1, vec2 p2) const;
+}
+
+///
+interface ElementDrawerInterface {
+ public:
+  ///
+  void Draw();
+}
